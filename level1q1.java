96ddf682b8d8dae5586f80396fe842d42e4c7e51
SG-mon156 <sg6625@srmist.edu.in>
2026-03-02 09:03:47 +0530

    Added level1q1 age calculation program

---
+++ level1q1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class level1q1{
+
+         public static void main(String[] args){
+                  int birthyear=2000;
+                  int currentyear=2024;
+                  int age= currentyear-birthyear;
+                  System.out.println("Harry's age in 2024 is" + age);
+}}
