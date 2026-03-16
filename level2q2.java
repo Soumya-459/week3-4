917b609572acf53fe8eec5025aeeb855c4f9691c
Soumya-459 <photik156@gmail.com>
2026-03-17 04:49:21 +0530

    Update level2q2.java

--- level2q2.java
+++ level2q2.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
 
-public class level2q2 {
+public class EmployeeBonus {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        // Input
-        double base = sc.nextDouble();
-        double height = sc.nextDouble();
+        double salary = sc.nextDouble();
+        int years = sc.nextInt();
 
-        // Area calculation
-        double areaSqCm = 0.5 * base * height;
-        double areaSqIn = areaSqCm / 6.4516;
+        double bonus = 0;
 
-        // Output
-        System.out.println("The Area of the triangle in sq in is " 
-                + areaSqIn + " and sq cm is " + areaSqCm);
+        if (years > 5) {
+            bonus = salary * 0.05;
+        }
 
-        sc.close();
+        System.out.println(bonus);
     }
 }
