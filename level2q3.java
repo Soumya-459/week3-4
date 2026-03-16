e73d200de8dbd675dea22607ffc9c5ff7a67dcf9
Soumya-459 <photik156@gmail.com>
2026-03-17 04:49:50 +0530

    Update level2q3.java

--- level2q3.java
+++ level2q3.java
@@ -1,19 +1,12 @@
 import java.util.Scanner;
 
-public class level2q3 {
+public class MultiplicationTable {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
 
-        // Input
-        double perimeter = sc.nextDouble();
-
-        // Calculation
-        double side = perimeter / 4;
-
-        // Output
-        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
-
-        sc.close();
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
     }
 }
