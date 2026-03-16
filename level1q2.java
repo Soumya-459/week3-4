a2423c2506c018ae7690f1bf3be35b0c3492f365
Soumya-459 <photik156@gmail.com>
2026-03-17 04:45:04 +0530

    Update level1q2.java

--- level1q2.java
+++ level1q2.java
@@ -1,9 +1,18 @@
-import java.util.*;
-public class level1q2{
-   public static void main(String[] args){
-     int maths=94;
-     int physics=95;
-     int chem=96;
-     double avg=(maths+chem+physics)/3;
-     System.out.println("Sam's average mark in PCM is " + avg);
-}}
+import java.util.*
+public class level1q1{
+
+         public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println(“enter three numbers”);
+int a= sc.nextInt();
+int b= sc.nextInt();
+int c= sc.nextInt();
+if (a>b && a>c){
+	System.out.println(“ Is the first number the smallest? yes”);
+}
+else{
+	System.out.println(“ Is the first number the smallest? no”);
+}
+sc.close();
+{
+}
