433030fadf2b50efb4abb7b6f885b5ed96e350cc
Soumya-459 <photik156@gmail.com>
2026-03-17 04:53:18 +0530

    Update level3q1.java

--- level3q1.java
+++ level3q1.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
 
-public class level3q1 {
+public class LeapYear {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
 
-        // Input
-        double celsius = sc.nextDouble();
+        if (year < 1582) {
+            System.out.println("Invalid Year");
+        } else {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        System.out.println("Leap Year");
+                    } else {
+                        System.out.println("Not a Leap Year");
+                    }
+                } else {
+                    System.out.println("Leap Year");
+                }
+            } else {
+                System.out.println("Not a Leap Year");
+            }
+        }
 
-        // Conversion
-        double fahrenheitResult = (celsius * 9 / 5) + 32;
-
-        // Output
-        System.out.println("The " + celsius + " celsius is " + fahrenheitResult + " fahrenheit");
-
-        sc.close();
+        if (year >= 1582 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
+            System.out.println("Leap Year");
+        } else if (year >= 1582) {
+            System.out.println("Not a Leap Year");
+        }
     }
 }
