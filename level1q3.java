0b30395e2229f12c1701f6a92d60022f769e0fa5
Soumya-459 <photik156@gmail.com>
2026-03-17 04:45:32 +0530

    Update level1q3.java

--- level1q3.java
+++ level1q3.java
@@ -1,7 +1,21 @@
-import java.util.*;
-public class level1q3{
-    public static void main(String[] args){
-      double km=10.8;
-      double miles=km*1.6;
-      System.out.println("The distance " + km + "km in miles is " + miles);
-}}
+import java.util.*
+public class level1q1{
+
+         public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println(“enter three numbers”);
+int a= sc.nextInt();
+int b= sc.nextInt();
+int c= sc.nextInt();
+if(a>b&&a>c){
+	System.out.println(“Is the first number the largest?yes Is the second number the largest? no Is the third number the largest? no”);
+}
+else if(b>a&&b>c){
+	System.out.println(“Is the first number the largest?no Is the second number the largest? yes Is the third number the largest? no”);
+}
+else{
+	System.out.println(“Is the first number the largest?no Is the second number the largest? no Is the third number the largest? yes”);
+}
+sc.close();
+}
+}
